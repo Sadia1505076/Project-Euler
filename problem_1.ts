f5e8672a4df48dafd39bc57b26c8034f262809de
Sadia1505076 <1505076.st@ugrad.cse.buet.ac.bd>
2020-08-19 11:33:17 +0600

    solving some style errors

--- problem_1.ts
+++ problem_1.ts
@@ -5,7 +5,7 @@ function findMultipleSum() : number {
     let sum: number = 0
 
     for ( i = 3 ; i < 1000 ; i++ ) {
-        if ( i % 3 ==0 || i % 5 == 0 ) {
+        if ( i % 3 == 0 || i % 5 == 0 ) {
             sum += i
         }
     }
