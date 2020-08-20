45c90a3705710e7cf6a461acb61b23a3287d0d53
Sadia1505076 <1505076.st@ugrad.cse.buet.ac.bd>
2020-08-20 16:42:29 +0600

    default time

--- problem_1.ts
+++ problem_1.ts
@@ -29,6 +29,7 @@ console.time()
 let multipleSum: number = findMultipleSum()
 console.timeEnd()
 // let t1                  = performance.now()
+// default 0.251ms
 
 console.log("Sum of multiple of 3 or 5 below 1000 is: ", multipleSum) // Answer is 233168
 // console.log("Call to the function took " + (t1 - t0) + " milliseconds.")
