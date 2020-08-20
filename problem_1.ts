d519faaf9284458f11bd992bce2245e61f795c32
Sadia1505076 <1505076.st@ugrad.cse.buet.ac.bd>
2020-08-20 12:50:27 +0600

    fixing style issues

--- problem_1.ts
+++ problem_1.ts
@@ -1,11 +1,22 @@
-// sum of multiple of 3 or 5 below 1000
+/*
+    Project Euler problem #1
+
+    Multiples of 3 and 5
+    ---------------------------------
+    
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    Find the sum of all the multiples of 3 or 5 below 1000.
+
+*/
+
 
 function findMultipleSum() : number {
-    let i: number
+
     let sum: number = 0
 
-    for ( i = 3 ; i < 1000 ; i++ ) {
-        if ( i % 3 == 0 || i % 5 == 0 ) {
+    for (let i: number = 3 ; i < 1000 ; i++) {
+
+        if (i % 3 == 0 || i % 5 == 0) {
             sum += i
         }
     }
@@ -13,5 +24,11 @@ function findMultipleSum() : number {
 }
 
 
-let multipleSum = findMultipleSum()
-console.log(multipleSum)
+// let t0                  = performance.now()
+console.time()
+let multipleSum: number = findMultipleSum()
+console.timeEnd()
+// let t1                  = performance.now()
+
+console.log("Sum of multiple of 3 or 5 below 1000 is: ", multipleSum) // Answer is 233168
+// console.log("Call to the function took " + (t1 - t0) + " milliseconds.")
