0f0fc0ad2d574bf0bed9e185c736836db5625b2b
Sadia1505076 <1505076.st@ugrad.cse.buet.ac.bd>
2020-08-20 12:44:54 +0600

    fixing some style issues

--- problem_3.ts
+++ problem_3.ts
@@ -1,28 +1,39 @@
-// find the largest prime factor of 600851475143 
+/*
+    Project Euler problem #3
+
+    Largest prime factor
+    -----------------------------
+
+    The prime factors of 13195 are 5, 7, 13 and 29.
+    What is the largest prime factor of the number 600851475143 ?
+
+*/ 
 
 
-function findLargestFactor ( givenNumber: number ) : number {   
-    let i: number
-    let j: number
-    let isPrime: boolean
+function findLargestFactor (givenNumber: number) : number {   
+
     let largestPrimeFactor: number = 0
+    let isPrime:            boolean
+ 
+    for (let i: number = 2 ; i < givenNumber ; i++) {
 
-    for ( i = 2 ; i < givenNumber ; i++ ) {
-        if ( givenNumber % i == 0 ) {
+        if (givenNumber % i == 0) {
+
             isPrime = true
-            for ( j = 2 ; j <= Math.sqrt(i) ; j++ ) {
-                if ( i % j == 0 ) {
+            for (let j: number = 2 ; j <= Math.sqrt(i) ; j++) {
+
+                if (i % j == 0) {
                     isPrime = false
                     break
                 } 
             } 
-            if ( isPrime == true && i > largestPrimeFactor ) largestPrimeFactor = i
+            if (isPrime == true && i > largestPrimeFactor) largestPrimeFactor = i
         }
     }
     return largestPrimeFactor
 }
 
 
-let givenNumber: number = 600851475143  
+let givenNumber:        number = 600851475143  
 let largestPrimeFactor: number = findLargestFactor (givenNumber) 
 console.log (largestPrimeFactor)
