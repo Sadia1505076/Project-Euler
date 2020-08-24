d7c047fb2f5c8f7f9891301ed62d8e9a2625d7a4
Sadia1505076 <1505076.st@ugrad.cse.buet.ac.bd>
2020-08-24 20:26:48 +0600

    new appraoch to find if the number is pallindrome and some optimization is done

--- problem_4.ts
+++ problem_4.ts
@@ -13,19 +13,33 @@
 
 let palindromeNumbers: Array<number> = [];
 
+function isPalindromeWithoutString (input: number) : boolean {
+    let reversedDigits:   Array<number> = [];
+    let quotient:         number = input;
+
+    while (quotient != 0) {
+        reversedDigits.push (quotient % 10);
+        quotient = Math.floor(quotient / 10);
+    }
+
+    let digits: Array<number> = [];
+    digits = digits.concat(reversedDigits).reverse();
+    return digits.every((val, index) => val == reversedDigits[index]);
+}
+
 function isPalindrome (input: number) : boolean {
     let stringFormedNumber: string = input.toString(10);
-    let reversedString:     string = stringFormedNumber.split('').reverse().join("");
-    
+    let reversedString:     string = stringFormedNumber.split('').reverse().join("");    
     if (stringFormedNumber != reversedString) return false; 
     return true;
 }
 
-function findLargestPalindrome (lowerBound: number, upperBound: number) : number {
+function deriveLargestPalindromeFromProduct (lowerBound: number, upperBound: number) : number {
     for (let i: number = lowerBound; i <= upperBound; i++){
         for (let j: number = i; j <= upperBound; j++){
             let potentialPalindrome: number = i * j; 
-            if (isPalindrome (potentialPalindrome)) {
+            if (potentialPalindrome < 100000) continue;
+            if (isPalindromeWithoutString (potentialPalindrome)) {
                 palindromeNumbers.push(potentialPalindrome);
             }
         }
@@ -36,8 +50,12 @@ function findLargestPalindrome (lowerBound: number, upperBound: number) : number
 
 
 console.time("time");
-let largestPalindrome: number = findLargestPalindrome(100, 999);
+let lowerBound:        number = 100;
+let upperBound:        number = 999;
+let largestPalindrome: number = deriveLargestPalindromeFromProduct (lowerBound, upperBound);
 console.timeEnd("time");
-//time: 288.390ms
+//previous time without optimization: 288.390ms
+// without string, time: 116.302ms
+// with string , time: 173.503ms
 console.log("Largest palindromic number made from the product of two 3-digit numbers is: ", largestPalindrome); 
 // Answer is 906609
