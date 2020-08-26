a4ebf5711c700b8a478b91aefb7eed24ed7e3c4b
Sadia1505076 <1505076.st@ugrad.cse.buet.ac.bd>
2020-08-26 13:36:17 +0600

    efficiency improved

--- problem_7.ts
+++ problem_7.ts
@@ -5,7 +5,7 @@
     ---------------------------------
     
     By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-    What is the 10 001st prime number?
+    What is the 10001st prime number?
 
 */
 
@@ -18,15 +18,43 @@ function isPrime (input: number) : boolean {
 }
 
 function calculateNthPrime (n: number) : number {
-    let countPrime: number = 0;
-    let iteration:  number = 2;
+    if (n == 1) return 2;
+    if (n == 2) return 3;
+    
+    let countPrime: number = 2;
+    let iteration:  number = 1;
+    let nthprime:   number;
+    let a:          number; // I didn't know how to name a and b.
+    let b:          number;
+    let isAPrime:   boolean;
+    let isBPrime:   boolean;
+
     while (countPrime < n) {
-        if (isPrime (iteration)) {
+        a = 6 * iteration - 1;
+        b = 6 * iteration + 1;
+        isAPrime = isPrime (a);
+        isBPrime = isPrime (b);
+        if (isAPrime && isBPrime) {
+            if (countPrime + 1 == n) {
+                return Math.min(a, b);
+            }
+            else {
+                nthPrime = Math.max(a, b);
+                countPrime += 2;
+            }
+        }
+        else if (isAPrime) {
+            nthPrime = a;
+            countPrime += 1;
+        }
+        else if (isBPrime){
+            nthPrime = b;
             countPrime += 1;
         }
         iteration ++;
     }
-    return iteration - 1;
+
+    return nthPrime;
 }
 
 
@@ -34,6 +62,8 @@ let n: number = 10001;
 console.time("time");
 let nthPrime: number = calculateNthPrime (n);
 console.timeEnd("time");
-//time: 25.883ms
 console.log("The 10001st prime number is: ", nthPrime);
-// answer is 104743
+
+// Time:       25.883ms
+// Answer:     104743
+// Time after: 25.217ms
